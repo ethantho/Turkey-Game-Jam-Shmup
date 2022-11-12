452d31779d98c7ebdfe3226bcc0bb592a352579c
ethantho <ethantho@umich.edu>
2022-11-12 01:37:30 -0500

    Focus mode

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float baseSpeed;
+    public float focusSpeed;
     private float speed;
     public int bulletDelay = 6;
     public int bulletDelayCounter = 0;
     public GameObject Bullet;
+    public GameObject LPod;
+    public GameObject RPod;
+    bool focusing;
     
     void Start()
     {
@@ -19,14 +23,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            focusMode();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            nonFocusMode();
+        }
+
+
+   
     }
 
 
     void FixedUpdate()
     {
+        if (!focusing)
+        {
+            speed = baseSpeed;
+        }
+        else
+        {
+            speed = focusSpeed;
+        }
         
-        speed = baseSpeed;
         if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0)
         {
             speed /= Mathf.Sqrt(2);
@@ -48,6 +69,8 @@ public class PlayerController : MonoBehaviour
             bulletDelayCounter++;
         }
 
+
+        
         
     }
 
@@ -55,5 +78,21 @@ public class PlayerController : MonoBehaviour
     {
         bulletDelayCounter = 0;
         Instantiate(Bullet, transform.position + new Vector3(0, 1f, 0), Quaternion.identity);
+        Instantiate(Bullet, RPod.transform.position, Quaternion.identity);
+        Instantiate(Bullet, LPod.transform.position, Quaternion.identity);
+    }
+
+    void focusMode()
+    {
+        LPod.transform.position += new Vector3(1, 1, 0);
+        RPod.transform.position += new Vector3(-1, 1, 0);
+        focusing = true;
+    }
+
+    void nonFocusMode()
+    {
+        LPod.transform.position += new Vector3(-1, -1, 0);
+        RPod.transform.position += new Vector3(1, -1, 0);
+        focusing = false;
     }
 }
