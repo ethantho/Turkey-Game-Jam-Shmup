83876def0f9024cfb01ef793de43b842acc0ab9b
alavacek <alavacek@umich.edu>
2022-11-11 22:34:54 -0500

    Basic Data Scripts

---
+++ Assets/Scenes/Scripts/AttackObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Attack/AttackObject")]
+
+//enum AttackTypes
+
+public class AttackObject : ScriptableObject
+{
+    [SerializeField] int AttackSpeed;
+    
+
+}
---
+++ Assets/Scenes/Scripts/Has Data/hasScore.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  ScoreEvent
+{
+    public int currentScore;
+
+    public ScoreEvent(int AddToScore)
+    {
+        this.currentScore = AddToScore;
+    }
+}
+
+public class hasScore : MonoBehaviour
+{
+    //should only be attached to player
+    [SerializeField] int score;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    public void Scored(int AddToScore)
+    {
+        score += AddToScore;
+        EventBus.Publish<ScoreEvent>(new ScoreEvent(AddToScore));
+    }
+
+}
